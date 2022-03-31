e941c1a01ae71e58f59616ee09f87ee2d2dc688a
JstRuslan <send2ruslan@gmail.com>
2022-03-31 19:30:56 +0300

    HomeWork1 <>

---
+++ src/main/java/less1/Apple.java
@@ -0,0 +1,11 @@
+package less1;
+
+public class Apple extends Fruit {
+
+    Apple(String color) {
+        name = "Apple";
+        weight = 1.0f;
+        this.color = color;
+
+    }
+}
---
+++ src/main/java/less1/Orange.java
@@ -0,0 +1,11 @@
+package less1;
+
+public class Orange extends Fruit {
+
+    Orange() {
+        name = "Orange";
+        weight = 1.5f;
+        color = "orange";
+
+    }
+}
